8d0b439c46384fdc582f6e6d80e4f8ae420123a1
Wisdom Dauda <codewithwise@gmail.com>
2025-06-10 19:23:56 +0100

    Routed My Server

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ import http from 'http';
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    
-    ///Chiling with Res
-    res.writeHead(200, { "Content-Type": 'application/json' })
-    res.end(JSON.stringify({message:"Wisdom is of God"}))
-    res.end();
+    //creating router
+    if (req.url == '/') {
+        res.writeHead(200, { "Content-Type": 'text/html' });
+        res.end('<h1>Welcome to  Micah Dauda</h1>')
+    } else
+        if (req.url == '/about') {
+            res.writeHead(200, { "Content-Type": 'text/html' });
+            res.end('<h1>About Micah Dauda</h1>')
+        }
 
+
+        else {
+
+        }
     //Chiling with Req
     console.log(req.url);
 
@@ -15,5 +23,5 @@ const server = http.createServer((req, res) => {
 
 
 server.listen(PORT, () => {
-    console.log("Running on"+PORT);
+    console.log("Running on" + PORT);
 })
