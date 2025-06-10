fb5909520cb2c451959485c466ecd4229468630e
Wisdom Dauda <codewithwise@gmail.com>
2025-06-10 18:42:02 +0100

    module

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
+import  getPosts  from "./postController.js";
+ 
+console.log(getPosts());
+/* const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
 
 console.log(`Random number: ${generateRandomNumber()}`)
-console.log(`32 from Farenheit: ${celciusToFahrenheit(32)}`)
+console.log(`32 from Farenheit: ${celciusToFahrenheit(32)}`)  */
