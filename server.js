97a6c486d92df80a333703d4ca8f353bbc4ccc10
Wisdom Dauda <codewithwise@gmail.com>
2025-06-10 19:17:20 +0100

    request

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ import http from 'http';
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(500, {"Content-Type":'application/json'})
+    
+    ///Chiling with Res
+    res.writeHead(200, { "Content-Type": 'application/json' })
     res.end(JSON.stringify({message:"Wisdom is of God"}))
     res.end();
+
+    //Chiling with Req
+    console.log(req.url);
 
 });
 
