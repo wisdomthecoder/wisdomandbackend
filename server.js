0f6ff5ee5ea871b855975dd49ac8d77759001f0c
Wisdom Dauda <codewithwise@gmail.com>
2025-06-10 19:29:45 +0100

    check fro methods

--- server.js
+++ server.js
@@ -2,22 +2,37 @@ import http from 'http';
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    //creating router
-    if (req.url == '/') {
-        res.writeHead(200, { "Content-Type": 'text/html' });
-        res.end('<h1>Welcome to  Micah Dauda</h1>')
-    } else
-        if (req.url == '/about') {
-            res.writeHead(200, { "Content-Type": 'text/html' });
-            res.end('<h1>About Micah Dauda</h1>')
+
+    try {
+        if (req.method == "GET") {
+            //creating router
+            if (req.url == '/') {
+                res.writeHead(200, { "Content-Type": 'text/html' });
+                res.end('<h1>Welcome to  Micah Dauda</h1>')
+            } else
+                if (req.url == '/about') {
+                    res.writeHead(200, { "Content-Type": 'text/html' });
+                    res.end('<h1>About Micah Dauda</h1>')
+                }
+
+
+                else {
+                    res.writeHead(400, { "Content-Type": 'text/html' });
+                    res.end('<h1>Not Found</h1>')
+
+                }
+            //Chiling with Req
+
+        } else {
+            throw new Error('Method not Allowed');
         }
+    } catch (error) {
+        res.writeHead(500, { "Content-Type": 'text/html' });
+        res.end('<h1>Server Error</h1>')
 
 
-        else {
+    }
 
-        }
-    //Chiling with Req
-    console.log(req.url);
 
 });
 
