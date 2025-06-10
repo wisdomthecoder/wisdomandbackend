d18c10821e7503753f63d51ca598a7e2161c1c98
Wisdom Dauda <codewithwise@gmail.com>
2025-06-10 18:25:29 +0100

    Celcius to Fah

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const generateRandomNumber = require('./utils');
+const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
 
 console.log(`Random number: ${generateRandomNumber()}`)
+console.log(`32 from Farenheit: ${celciusToFahrenheit(32)}`)
--- utils.js
+++ utils.js
@@ -1,5 +1,11 @@
 function generateRandomNumber() {
     return Math.floor(Math.random() * 100) + 1;
 }
-
-module.exports = generateRandomNumber; 
+function celciusToFahrenheit(ceilcius) {
+    return (ceilcius * 9) / 5 + 32;
+}
+module.exports = {
+    generateRandomNumber,
+    celciusToFahrenheit 
+    
+};  
