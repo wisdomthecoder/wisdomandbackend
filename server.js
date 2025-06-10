0fbf5a08dc45c72ce3e82e7ae63c0fa7879ed6e4
Wisdom Dauda <codewithwise@gmail.com>
2025-06-10 18:53:36 +0100

    content type and status code

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import http from 'http';
 const PORT = 2005;
 
 const server = http.createServer((req, res) => {
-    res.write("Wisdom Will Do Backend");
+    res.setHeader("Content-Type", 'text/html'); 
+    res.statusCode = 404;
+    res.write("<h1>Wisdom Will Do Backend</h1>");
     res.end();
 
 });
