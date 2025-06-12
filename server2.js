526ecf8f21aa5325f4fbd3b5ddf74e4c1672e3e2
Wisdom Dauda <codewithwise@gmail.com>
2025-06-12 20:59:37 +0100

    Server gets my siblings

--- server2.js
+++ server2.js
@@ -1,23 +1,37 @@
 import http from 'http';
 
 const PORT = process.env.PORT;
-const user = [
+const users = [
     { id: 1, name: 'Wisdom Dauda' },
     { id: 2, name: 'Kwopnan Dauda' },
     { id: 3, name: 'Dangriang Dauda' }
 ];
 
+//Logger Urel
+
+//
+
 
 const server = http.createServer((req, res) => {
     if (req.url == '/api/users' && req.method == "GET") {
         res.setHeader('Content-Type', 'application/json')
-        res.write(JSON.stringify(user))
-        res.end();
-    } else if (req.url.match(/\/api\/users\/([0-9]+)/)) {
-         res.setHeader('Content-Type', 'application/json')
-        res.write(JSON.stringify(user[req.url.split("/")[req.url.split("/").length-1]]))
+        res.write(JSON.stringify(users))
         res.end();
 
+    } else if (req.url.match(/\/api\/users\/([0-9]+)/)) {
+        const id = req.url.split("/")[3];
+        const user = users.find((user) => user.id == parseInt(id));
+
+        if (user) {
+
+            res.setHeader('Content-Type', 'application/json')
+            res.write(JSON.stringify(user))
+
+        } else {
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+            res.end("Users not Found")
+        }
+        res.end();
     } else {
         res.writeHead(404, { 'Content-Type': 'application/json' })
         res.end("Route not Found")
