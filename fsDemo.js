2279db1f13534e8a6ceb96d9991de166ca7dd69d
Wisdom Dauda <codewithwise@gmail.com>
2025-06-12 21:48:50 +0100

    Promises .then()

--- fsDemo.js
+++ fsDemo.js
@@ -1,13 +1,21 @@
-import fs from "fs";
+import fs from 'fs/promises';
+
+// import fs from "fs";
 
 
-//callback
-fs.readFile('./text.txt', 'utf-8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-});
+// //callback
+// fs.readFile('./text.txt', 'utf-8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
 
-//ReadFileSync - Synchronous version
+// //ReadFileSync - Synchronous version
 
-const data = fs.readFileSync('./text.txt', 'utf8');
-console.log(data);
+// const data = fs.readFileSync('./text.txt', 'utf8');
+// console.log(data);
+
+
+//Promises .then()
+fs.readFile('./text.txt', 'utf8')
+    .then((data) => { console.log(data) })
+    .catch((err)=>console.log(err));
