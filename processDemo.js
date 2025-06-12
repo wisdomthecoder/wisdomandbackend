b3838f51f0f364b38a57ab0e02660d4aa3bf29a0
Wisdom Dauda <codewithwise@gmail.com>
2025-06-12 22:58:07 +0100

    process

---
+++ processDemo.js
@@ -0,0 +1,23 @@
+console.log(process.argv)
+console.log(process.argv[3])
+
+
+//process.env
+console.log(process.env.LOGNAME)
+
+//process.env
+console.log(process.pid);
+
+//cwd
+console.log(process.cwd())
+
+//title
+console.log(process.title);
+
+//mem usage
+console.log(process.memoryUsage());
+
+//up time
+console.log(process.uptime())
+
+process.exit(0x0)
